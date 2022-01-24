8b32d8d04323a530d4a13b60967744e96dc8a4e1
Ashish Mainali <ashishmainali@bajratechnologies.com>
2022-01-24 11:13:25 +0545

    feature: associated posts with user adding user id to posts.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_21_065808) do
+ActiveRecord::Schema.define(version: 2022_01_21_083703) do
 
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
@@ -55,6 +55,8 @@ ActiveRecord::Schema.define(version: 2022_01_21_065808) do
     t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
