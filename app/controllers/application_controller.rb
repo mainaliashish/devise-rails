0b1cbf97f63e32d79691edca4e9b9cb5e3300427
Ashish Mainali <ashishmainali@bajratechnologies.com>
2022-01-24 15:53:49 +0545

    Fix: added custom logout action on users and ability to update post based on associated user or admin.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,16 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :authenticate_user!
 
   protected
+
+  def after_sign_in_path_for(resource)
+    posts_path
+  end
+
+  def after_sign_out_path_for(resource)
+    flash[:notice] = 'Logged out successfully.'
+    root_path
+  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name username])
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,24 +1,20 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_post, only: %i[show edit update destroy]
+  before_action :require_same_user_or_admin, except: %i[index new create show]
 
-  # GET /posts or /posts.json
   def index
-    @posts = Post.all
+    @posts = Post.order(created_at: :desc)
   end
 
-  # GET /posts/1 or /posts/1.json
   def show; end
 
-  # GET /posts/new
   def new
     @post = Post.new
   end
 
-  # GET /posts/1/edit
   def edit; end
 
-  # POST /posts or /posts.json
   def create
     @post = Post.new(post_params)
     respond_to do |format|
@@ -32,7 +28,6 @@ class PostsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /posts/1 or /posts/1.json
   def update
     respond_to do |format|
       if @post.update(post_params)
@@ -45,7 +40,6 @@ class PostsController < ApplicationController
     end
   end
 
-  # DELETE /posts/1 or /posts/1.json
   def destroy
     @post.destroy
 
@@ -63,5 +57,12 @@ class PostsController < ApplicationController
 
   def post_params
     params.require(:post).permit(:title, :content).merge(user: current_user)
+  end
+
+  def require_same_user_or_admin
+    return if current_user == @post.user || current_user.admin?
+
+    flash[:notice] = 'You can only perform action on your own articles.'
+    redirect_to posts_path
   end
 end
