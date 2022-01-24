96955d05e2ac5e005306276cc1ec5e4fd08150ce
Ashish Mainali <ashishmainali@bajratechnologies.com>
2022-01-24 16:30:00 +0545

    Fix: added action based on users roles with user restrictions. Guest users can't see edit/destroy action.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!, except: :index
+  before_action :authenticate_user!, except: %i[index show]
   before_action :set_post, only: %i[show edit update destroy]
   before_action :require_same_user_or_admin, except: %i[index new create show]
 
