9c81587e2235b3821ccc20c6aa48ba95077248c3
Ashish Mainali <ashishmainali@bajratechnologies.com>
2022-01-25 13:14:15 +0545

    feature: Preview Uploaded photo
    User can see the preview of the photo they uploaded before updating profile.

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,21 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+
+const selectUserImage = document.querySelector("#userPictureInput");
+
+selectUserImage.addEventListener("change", (event) => {
+  let files = event.target.files;
+  let image = files[0];
+  let reader = new FileReader();
+  reader.onload = function (file) {
+    let img = new Image();
+    console.log(file);
+    img.src = file.target.result;
+    // console.log(img.src);
+    document.querySelector("#old-image").src = img.src;
+  };
+  reader.readAsDataURL(image);
+  // console.log(files);
+});
